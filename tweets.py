94db7ad8063edefafa66cbaddee8de07cc2abf83
Etienne Doumazane <etienne.doumazane@gmail.com>
2021-08-04 02:21:56 +0200

    Update

--- tweets.py
+++ tweets.py
@@ -1,42 +1,31 @@
 tweets = [
-    "[🤖 ThreadFromATextFile] Here I show how you can write a thread in a txt file and publish it.",
-    "This thread itself has been generated with this method. Noteworthily, the numeration of the tweets is automatic.",
-    "First, have a look at the text file. It contains two lists: tweets and media",
-    "'tweets' is a list of strings. Each string in tweets is one tweet",
+    "Hi! \nHere I show how you can write a thread in a txt file and publish it. 🧵 ⬇️⬇️⬇️ \n#python #twitterapi",
+    "This thread itself has been generated automatically. \nNote that the numeration of the tweets is also automatic!",
+    "First, have a look at the text file ⤵️. It contains two lists: 'tweets' and 'media'",
+    "1️⃣ 'tweets' is a list of strings. Each string is one tweet",
 
-    "A tweet can contain a URL, for example the address to this project's repo: ",
-    "'media' is a list of lists. It contains the relative paths (strings) of up to 4 PNG/JPG files (or 1 GIF file) you want to embed.",
-    "There's as many elements in tweets as in media: you should use an empty list if you don't want to embed an image.",
-    "Next, have a look at the script. Here in a Jupyter notebook: "
+    "A tweet can contain a URL, for example the address to this project's repo: https://github.com/E-Dmz/ThreadFromATextFileScript",
+    "2️⃣ 'media' is a list of lists. It contains the relative paths (strings) of up to 4 PNG/JPG files (or 1 GIF file) you want to embed.",
+    "There should be as many elements in 'tweets' as in 'media': use an empty list if you don't want to embed an image in a tweet.",
+    "⏩ Now, have a look at the script ⤵️.",
 
-
+    "It loops across the tweets. The first tweet is different (the beginning and the end differ). \n\
+Most importantly, the first tweet is not a reply to another tweet ('None'), while the others are replies to the previous one ('status.id').",
+    "Do you like this method? Please try it, comment, retweet and follow me! #python #twitterapi",
 ]
 
 media = [
-    ["../Output/Type0/France-incidence hebdo.png", "../Output/Type0/France-taux hosp.png", "../Output/Type0/France-taux rea.png", "../Output/Type0/France-taux décès.png"],
-    ["../Output/Type1/régions métropolitaines 1 sur 3, classées par taux de réanimation décroissant chez les personnes de 30 à 59 ans.png"],
-    ["../Output/Type1/régions métropolitaines 2 sur 3, classées par taux de réanimation décroissant chez les personnes de 30 à 59 ans.png"], 
-    ["../Output/Type1/régions métropolitaines 3 sur 3, classées par taux de réanimation décroissant chez les personnes de 30 à 59 ans.png"],
+    [],
+    [],
+    ['Screenshot_tweets_and_media.png'], 
+    [],
     
     [],
-    ["../Output/Type1/zoom régions métropolitaines 1 sur 3, classées par taux de réanimation décroissant chez les personnes de 30 à 59 ans.png"],
-    ["../Output/Type0/Occitanie-incidence hebdo.png","../Output/Type0/Occitanie-taux hosp.png","../Output/Type0/Occitanie-taux rea.png","../Output/Type0/Occitanie-taux décès.png"],
-    ["../Output/Type1/zoom régions métropolitaines 2 sur 3, classées par taux de réanimation décroissant chez les personnes de 30 à 59 ans.png", "../Output/Type1/zoom régions métropolitaines 3 sur 3, classées par taux de réanimation décroissant chez les personnes de 30 à 59 ans.png"],
-    # ["../Output/Type0/Corse-incidence hebdo.png", "../Output/Type0/Corse-taux hosp.png", "../Output/Type0/Corse-taux rea.png", "../Output/Type0/Corse-taux décès.png"],
-    # ["../Output/Type0/Occitanie-incidence hebdo.png", "../Output/Type0/Occitanie-taux hosp.png", "../Output/Type0/Occitanie-taux rea.png", "../Output/Type0/Occitanie-taux décès.png"],
-    # ["../Output/Type0/Provence-Alpes-Côte d'Azur-incidence hebdo.png", "../Output/Type0/Provence-Alpes-Côte d'Azur-taux hosp.png", "../Output/Type0/Provence-Alpes-Côte d'Azur-taux rea.png", "../Output/Type0/Provence-Alpes-Côte d'Azur-taux décès.png"],
-    # ["../Output/Type0/Île-de-France-incidence hebdo.png", "../Output/Type0/Île-de-France-taux hosp.png", "../Output/Type0/Île-de-France-taux rea.png", "../Output/Type0/Île-de-France-taux décès.png"],
-    ["../Output/Type1/régions d'outre-mer, classées par taux de réanimation décroissant chez les personnes de 30 à 59 ans.png"],
+    ['1.png', '2.png', '3.png', '4.png'],
     [],
-    ["../Output/Type0/Martinique-incidence hebdo.png", "../Output/Type0/Martinique-taux hosp.png", "../Output/Type0/Martinique-taux rea.png", "../Output/Type0/Martinique-taux décès.png"],
+    ['Screenshot_notebook.png'],
+
     [],
-    # ["../Output/Type0/Guyane-incidence hebdo.png", "../Output/Type0/Guyane-taux hosp.png", "../Output/Type0/Guyane-taux rea.png", "../Output/Type0/Guyane-taux décès.png"],
-    # ["../Output/Type0/Mayotte-incidence hebdo.png", "../Output/Type0/Mayotte-taux hosp.png", "../Output/Type0/Mayotte-taux rea.png", "../Output/Type0/Mayotte-taux décès.png"],
-    # [],
-    # [],
-    # [],
-    # [],
-    # [],
-    # [],
-    # [],
+    [],
 ]
+
